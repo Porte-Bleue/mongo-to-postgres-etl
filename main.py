b673e6f36a83cb9bda436bbe1a55a3b919b10fea
Louis Guitton <louisguitton93@gmail.com>
2022-01-23 19:14:34 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+from bson.json_util import dumps
+import json
+
+import pymongo
+from dotenv import load_dotenv
+from sqlmodel import Session, SQLModel, create_engine
+from models import Products
+
+load_dotenv()
+
+orm_classes = {
+    "products": Products
+    # "families": Families
+}
+
+
+def extract(table_name: str = "products"):
+    """Extract data from MongoDB to local JSONLine file.
+
+    Ref: https://realpython.com/introduction-to-mongodb-and-python/
+    """
+    client = pymongo.MongoClient(os.environ.get("MONGO_DB"))
+    db = client.porteBleue  # use porteBleue
+    table = db[table_name]  # db.getCollection("products")
+
+    with open(f"data/{table_name}.jsonl", "w") as fh:
+        fh.writelines([dumps(doc) + "\n" for doc in table.find()])
+
+
+def load(table_name: str = "products"):
+    """Load data from local JSONLine file to PostgresSQL."""
+    engine = create_engine(os.environ.get("POSTGRES_DB"))
+    SQLModel.metadata.create_all(engine)
+
+    with open(f"data/{table_name}.jsonl", "r") as fh:
+        Class = orm_classes[table_name]
+        objects = [Class(id=i, **json.loads(p)) for i, p in enumerate(fh.readlines())]
+
+    # TODO: instead of writing all objects, only do an incremental write
+    with Session(engine) as session:
+        session.add_all(objects)
+        session.commit()
+
+
+if __name__ == "__main__":
+    tables_to_extract = [
+        "products"
+        # "families"
+    ]
+    for t in tables_to_extract:
+        print(f"extracting table: {t}")
+        extract(table_name=t)
+        print(f"loading table: {t}")
+        load(table_name=t)
---
+++ models.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from pydantic import validator
+from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+
+def to_camel(name):
+    return "".join(
+        x.capitalize() if i > 0 else x for i, x in enumerate(name.split("_"))
+    )
+
+
+class Products(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    unit_of_measure: str
+    quantity_for_one_foodstuff: int
+    created_at: datetime
+    updated_at: datetime
+    created_by: str
+    category: str
+    cupboard: str
+    updated_by: str
+    current_stock: int
+
+    @validator("created_by", "category", "cupboard", "updated_by", pre=True)
+    def unnest_id(cls, v):
+        return v["$oid"]
+
+    @validator("created_at", "updated_at", pre=True)
+    def unnest_date(cls, v):
+        return v["$date"]
+
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+
+
+# class Categories
+
+# class Families
