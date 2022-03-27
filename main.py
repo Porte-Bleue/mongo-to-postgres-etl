cdd5cf19ecc53406f27477c9a11b47ab5b6b3b3c
Marie Lestavel <marielestavel@gmail.com>
2022-03-27 03:24:37 +0200

    Review changes

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-import os
 from bson.json_util import dumps
 import json
 from datetime import datetime, timedelta
 
 import pymongo
 import sqlalchemy
-from dotenv import load_dotenv
 from sqlmodel import Session, SQLModel, create_engine
-import prefect
 from prefect import task, Flow
-from prefect.client.secrets import Secret
+from prefect.tasks.secrets import PrefectSecret
 from models import Products, Families, FamilyMembers, VisitEvents, Operations
-
-load_dotenv()
 
 orm_classes = {
     "products": Products,
@@ -22,18 +17,14 @@ orm_classes = {
     "operations": Operations, 
 }
 
-logger = prefect.context.get("logger")
-
 @task
-def extract(collection_name: str):
+def extract(mongodb_secret: str, collection_name: str):
     """Extract data from MongoDB to local JSONLine file.
 
     Ref: https://realpython.com/introduction-to-mongodb-and-python/
     """
-    mongodb_secret = Secret("PREFECT__MONGO_DB")
-    logger.info(mongodb_secret.get())
-    client = pymongo.MongoClient(os.environ.get("PREFECT__MONGO_DB"))
-    db = client.porteBleue  # use porteBleue
+    client = pymongo.MongoClient(mongodb_secret) # Connect to MongoDB instance
+    db = client.porteBleue  # Access `porteBleue` database
     collection = db[collection_name]  # db.getCollection("products")
 
     with open(f"data/{collection_name}.jsonl", "w") as fh:
@@ -43,17 +34,15 @@ def extract(collection_name: str):
             fh.writelines([dumps(doc) + "\n" for doc in collection.find()])
 
 @task
-def load(table_name: str):
+def load(postgre_secret: str, table_name: str):
     """Load data from local JSONLine file to PostgresSQL."""
-    postgre_secret = Secret("PREFECT__POSTGRES_DB")
-    logger.info(postgre_secret.get())
-    engine = create_engine(os.environ.get("PREFECT__POSTGRES_DB"))
-    Class = orm_classes[table_name]
-    connection = engine.connect()
-    sql_query = sqlalchemy.text(f"drop table if exists {table_name} cascade;")
-    connection.execute(sql_query)
-    connection.commit()
-    SQLModel.metadata.create_all(engine)
+    engine = create_engine(postgre_secret) # Create engine to connect to Postgre database
+    Class = orm_classes[table_name] 
+    connection = engine.connect() # Connect to the database
+    sql_query = sqlalchemy.text(f"drop table if exists {table_name} cascade;") # Transaction to drop the table and its dependent objects. 
+    connection.execute(sql_query) # Execute transaction
+    connection.commit() # Commit the transaction
+    SQLModel.metadata.create_all(engine) 
 
     with open(f"data/{table_name}.jsonl", "r") as fh:
         objects = [Class(id=i, **json.loads(p)) for i, p in enumerate(fh.readlines())]
@@ -74,11 +63,14 @@ if __name__ == "__main__":
         "operations"
     ]
     with Flow("ETL From MongoDB To Postgres") as flow:
+        # Load credentials stored as Secrets on Prefect Cloud
+        mongodb_secret = PrefectSecret("PREFECT__MONGO_DB")
+        postgre_secret = PrefectSecret("PREFECT__POSTGRES_DB")
         for t in tables_to_extract:
             print(f"extracting collection: {t}")
-            t1 = extract(collection_name=t)
+            t1 = extract(mongodb_secret, collection_name=t)
             print(f"loading table: {t}")
-            t2 = load(table_name=t, upstream_tasks=[t1])
+            t2 = load(postgre_secret, table_name=t, upstream_tasks=[t1])
 
 # Deploy Prefect flow
 flow.register(project_name="analytics-la-porte-bleue")
