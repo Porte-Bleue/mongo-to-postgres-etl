ee7bdec60ddf3541c0f83a0804e3c02adfd37359
marielestavel <marielestavel@gmail.com>
2023-02-17 15:32:16 +0100

    Update age_range BAPIF

--- dbt-models/models/food_bank_report/yearly_report/viz_food_bank_report__family_members__yearly.sql
+++ dbt-models/models/food_bank_report/yearly_report/viz_food_bank_report__family_members__yearly.sql
@@ -33,7 +33,7 @@ family_members_yearly as (
             when age >= 0 and age <= 3 then '0 to 3'
             when age >= 4 and age <= 14 then '4 to 14'
             when age >= 15 and age <= 25 then '15 to 25'
-            when age >= 26 and age <= 64 then '16 to 64'
+            when age >= 26 and age <= 64 then '26 to 64'
             when age >= 65 then '65+'
         end as age_range,
         count(family_member_id) as total_people_helped
