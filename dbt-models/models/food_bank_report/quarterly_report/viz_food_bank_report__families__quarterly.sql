853f71ab0388377f7963bdcf1ee81cba2d9d7603
marielestavel <marielestavel@gmail.com>
2023-03-31 16:31:50 +0200

    Add max_weekly_visits to viz_food_bank_report__families__quarterly

--- dbt-models/models/food_bank_report/quarterly_report/viz_food_bank_report__families__quarterly.sql
+++ dbt-models/models/food_bank_report/quarterly_report/viz_food_bank_report__families__quarterly.sql
@@ -1,6 +1,26 @@
-with family_members__visit_events as (
+with families_visit_events as (
 
     select * from {{ ref('stg_families__visit_events') }}
+
+),
+
+weekly_visits as (
+
+    select
+        date_trunc('quarter', visit_date)::date as quarter_at,
+        date_trunc('week', visit_date)::date as week_at,
+        count(visit_id) as total_visits
+    from families_visit_events
+    group by 1,2
+),
+
+max_weekly_visits_by_quarter as (
+
+    select
+        quarter_at,
+        max(total_visits) as max_weekly_visits
+    from weekly_visits
+    group by 1
 
 ),
 
@@ -12,9 +32,20 @@ metrics_by_quarter as (
         count(distinct family_id) as family_count,
         count(distinct visit_id) as total_visits,
         sum(number_of_operations) as total_product_distributed
-    from family_members__visit_events
+    from families_visit_events
     group by 1
+
+),
+
+joined as (
+
+    select
+        metrics_by_quarter.*,
+        max_weekly_visits
+    from metrics_by_quarter
+    left join max_weekly_visits_by_quarter
+        on metrics_by_quarter.quarter_at = max_weekly_visits_by_quarter.quarter_at
 
 )
 
-select * from metrics_by_quarter
+select * from joined
